acc6aa96a623007698d5ee3577e2ee9f94c7e3ef
Matt Thompson <mattluthompson@gmail.com>
2022-08-23 12:42:36 -0400

    remove unecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('This is a test message!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
